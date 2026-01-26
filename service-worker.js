ab27bbdd03eedca1dcbc8e7e982a28178bbb294c
Hesh-0901 <henochshungu@gmail.com>
2026-01-26 23:14:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
 const CACHE_NAME = "become-v1";
-const urlsToCache = [
+
+const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
-  "/manifest.json"
+  "/user.html",
+  "/manifest.json",
+  "/icons/icon.192.png",
+  "/icons/icon.512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
